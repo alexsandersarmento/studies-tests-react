df8a9a14917eb77f77a5bd15c32af891d05ddf0c
Alexsander <alexsander.br18@gmail.com>
2023-05-29 11:30:32 -0300

    feat: created the handleSubmit function

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { FormEvent } from "react";
 import LoginForm from "../../components/LoginForm";
 import "./styles.css";
 
 const Login: React.FC = () => {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log('OnSubmit');
+  };
+
   return (
     <div className="container">
       <h1 data-testid="login-title">Login</h1>
 
-      <LoginForm onSubmit={() => {}} />
+      <LoginForm onSubmit={handleSubmit} />
     </div>
   )
 };
